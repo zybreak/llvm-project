4cee8631bc6b584e999190966360d7abfb74e84a
Sergei Barannikov <barannikov88@gmail.com>
2024-08-01 23:47:25 +0300

    [IR] Make @llvm.memset prototype byte width dependent
    This patch changes the type of the value argument of @llvm.memset and
    similar intrinsics from i8 to iN, where N is the byte width specified
    in data layout string.
    Note that the argument still has fixed type (not overloaded), but type
    checker will complain if the type does not match the byte width.
    
    Ideally, the type of the argument would be dependent on the address
    space of the pointer argument. It is easy to do this (and I did it
    downstream as a PoC), but since data layout string doesn't currently
    allow different byte widths for different address spaces, I refrained
    from doing it now.

--- llvm/include/llvm-c/Core.h
+++ llvm/include/llvm-c/Core.h
@@ -2823,7 +2823,7 @@ LLVMValueRef LLVMGetIntrinsicDeclaration(LLVMModuleRef Mod,
  *
  * @see llvm::Intrinsic::getType()
  */
-LLVMTypeRef LLVMIntrinsicGetType(LLVMContextRef Ctx, unsigned ID,
+LLVMTypeRef LLVMIntrinsicGetType(LLVMModuleRef Mod, unsigned ID,
                                  LLVMTypeRef *ParamTypes, size_t ParamCount);
 
 /**
